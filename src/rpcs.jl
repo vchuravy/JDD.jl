e210c43cbe7d98840fa8dbf7a4d3b22e8bdde8e7
Valentin Churavy <v.churavy@gmail.com>
2019-07-04 17:32:06 -0400

    fix some issues

--- src/rpcs.jl
+++ src/rpcs.jl
@@ -12,20 +12,35 @@ const _msg_id = Ref{Int}(0)
 msg_id() = _msg_id[] += 1
 
 function dispatch(rpc, ws)
-    resp = @match rpc.method begin
-        "Runtime.enable"  => Runtime.enable()
-        "Runtime.runIfWaitingForDebugger"  => Runtime.runIfWaitingForDebugger()
-        "Debugger.setPauseOnExceptions" => Debugger.setPauseOnExceptions(rpc.params)
-        "Debugger.setAsyncCallStackDepth" => Debugger.setAsyncCallStackDepth(rpc.params)
-        "Debugger.setBlackboxPatterns" => Debugger.setBlackboxPatterns(rpc.params)
-        "Debugger.setBreakpointsActive" => Debugger.setBreakpointsActive(rpc.params)
-        "Debugger.setBreakpointByUrl"  => Debugger.setBreakpointByUrl(rpc.params)
-        "Debugger.enable" => Debugger.enable()
-        _                 => dispatch_unhandled(rpc)
+    meth = @match rpc.method begin
+        "Runtime.enable"                    => Runtime.enable
+        "Runtime.runIfWaitingForDebugger"   => Runtime.runIfWaitingForDebugger
+        "Runtime.getHeapUsage"              => Runtime.getHeapUsage
+        "Debugger.setPauseOnExceptions"     => Debugger.setPauseOnExceptions
+        "Debugger.setAsyncCallStackDepth"   => Debugger.setAsyncCallStackDepth
+        "Debugger.setBlackboxPatterns"      => Debugger.setBlackboxPatterns
+        "Debugger.setBreakpointsActive"     => Debugger.setBreakpointsActive
+        "Debugger.setBreakpointByUrl"       => Debugger.setBreakpointByUrl
+        "Debugger.enable" => Debugger.enable
+        _                 => nothing 
+    end
+    resp = if meth === nothing
+        Base.invokelatest(dispatch_unhandled, rpc)
+    else
+        try 
+        if isdefined(rpc, :params)
+            Base.invokelatest(meth, rpc.params)
+        else
+            Base.invokelatest(meth)
+        end
+        catch e
+            Base.showerror(stderr,e,Base.catch_backtrace())
+            ()
+        end
     end
 
     # each response has to hve the same id as the source
-    resp = merge((;id = rpc.id), resp)
+    resp = (;id = rpc.id, result = resp)
     @show resp
     io = IOBuffer()
     JSON2.write(io, resp)
@@ -39,7 +54,7 @@ end
 
 
 module Debugger
-    enable() = (;debuggerId = "0")
+    enable(params) = (;debuggerId = "0")
     setPauseOnExceptions(params) = ()
     setAsyncCallStackDepth(params) = ()
     setBlackboxPatterns(params) = ()
@@ -53,4 +68,5 @@ module Runtime
     enable() = ()
     runIfWaitingForDebugger() = ()
     getIsolateId() = ()
+    getHeapUsage(params)= (;usedSize = 1024, totalSize = 4096)
 end
